4034c738acc6508d7afc5de06750a56e5092317e
atharvamk14 <56110309+atharvamk14@users.noreply.github.com>
2024-05-02 14:22:28 -0400

    directory restructure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="Hello-App">
+      <header className="Welcome">
+        <p>Hello, World!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
